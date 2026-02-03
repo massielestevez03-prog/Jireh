52e41e2537da9fe72e7dded6a0ebe0f9f4de7aee
massielestevez03-prog <massielestevez03@gmail.com>
2026-02-03 11:53:53 -0400

    Delete app directory

--- app/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, 
-  /* Si usas imágenes de dominios externos en el futuro, agrégalos aquí */
-}
-
-module.exports = nextConfig
