5e040ecbb8f948799780004245ebd9fa901d01c2
massielestevez03-prog <massielestevez03@gmail.com>
2026-02-03 11:52:25 -0400

    Create next.config.js

---
+++ app/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, 
+  /* Si usas imágenes de dominios externos en el futuro, agrégalos aquí */
+}
+
+module.exports = nextConfig
